78b9ddabde9ae5ef8e51b6a0041178699b73fc3f
bsegers <briansegs@gmail.com>
2024-04-05 19:26:33 -0400

    add: Setup App

---
+++ bank_app/src/App.jsx
@@ -0,0 +1,14 @@
+import styles from "./style";
+
+const App = () => (
+  <div className='bg-primary w-full overflow-hidden'>
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        Navbar
+      </div>
+    </div>
+  </div>
+);
+
+
+export default App
